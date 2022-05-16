1ffae3a54ba86d17f1f1b3fb28678f7c5758b1d0
weirdbb91 <weirdbb91@gmail.com>
2022-05-16 14:41:35 +0900

    [Feat] ✨ oakpos 컨테이너 실행시 DB 변경사항 반영, DB 초기값 추가
    DB 초기값 위치 - api/migrations/*.json

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,5 +4,8 @@ dockerize -wait tcp://$1:$2 -timeout 60s
 
 echo "database connected"
 
+python manage.py makemigrations
 python manage.py migrate
+python manage.py loaddata api/migrations/*.json
+
 python manage.py runserver 0.0.0.0:8000
