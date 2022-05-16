ba20dd4b92aefa3c03fe75f386cf8ebb3bc8af0e
weirdbb91 <weirdbb91@gmail.com>
2022-05-16 14:40:20 +0900

    [Feat] 📦️ 멤버 테이블 추가

--- api/models.py
+++ api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+class TimestampedModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="생성 시각")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="수정 시각")
+    
+    class Meta:
+        abstract = True
+
+
+class Member(TimestampedModel):
+    name = models.CharField(null=False, max_length=255, verbose_name="이름")
+    is_guest = models.BooleanField(null=False, default=True, verbose_name="손님 여부")
+    
+    class Meta:
+        ordering = ["is_guest", "id"]
+        verbose_name = "멤버"
+        verbose_name_plural = "멤버 리스트"
+    
+    def __str__(self):
+        return self.name
