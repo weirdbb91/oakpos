3a57278e5ac497aebd1e985b90618a1f65f5790f
weirdbb91 <weirdbb91@gmail.com>
2022-05-17 22:38:05 +0900

    [Docs] 📝 쿼리 로그 출력 설정

--- oakpos/settings.py
+++ oakpos/settings.py
@@ -139,3 +139,26 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+LOGGING = {
+    'version': 1,
+    'filters': {
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        }
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+        }
+    },
+    'loggers': {
+        'django.db.backends': {
+            'level': 'DEBUG',
+            'handlers': ['console'],
+        }
+    }
+}
