2815b00b42e10c4a9145b00cac5322499b4d3bb4
weirdbb91 <weirdbb91@gmail.com>
2022-05-17 22:57:40 +0900

    [Feat] ✨ 카테고리 Serializer 추가, 같은 레벨에서 이름 중복 검증

--- api/serializers.py
+++ api/serializers.py
@@ -24,3 +24,34 @@ class MemberSerializers():
     
     class Create(Retrieve):
         pass
+
+
+class CategorySerializers():
+    class List(serializers.ModelSerializer):
+        name = serializers.CharField(required=True, max_length=50)
+        description = serializers.CharField(allow_null=True, allow_blank=True)
+        parent = serializers.PrimaryKeyRelatedField(allow_null=True, read_only=True)
+        
+        class Meta:
+            model = models.Category
+            fields = "__all__"
+
+    class Update(List):
+        parent = serializers.PrimaryKeyRelatedField(allow_null=True, queryset=models.Category.objects.all())
+        def validate(self, data):
+            conds = {
+                "parent_id": data["parent"].id if data["parent"] else None,
+                "name": data["name"],
+            }
+            if models.Category.objects.filter(**conds).exists():
+                raise serializers.ValidationError(
+                    {"name": f"같은 레벨의 카테고리에 이미 존재하는 이름입니다."}
+                )
+            return data
+
+
+    class Retrieve(Update):
+        pass
+
+    class Create(Retrieve):
+        pass
